3f74eafbf2bf782d723b9a327f98453c68b2bc0d
Julia Koroleva <julikorolevich94@gmail.com>
2021-09-28 20:50:38 +0300

    Added Lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson1
+{
+    /// <summary>
+    /// Описание класса
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        /* запрос имени пользователя и отображение текущей даты после ввода имени
+          без проверок, что пользователь заполнил имя */
+        {
+            Console.Write("Добрый день! Введите Ваше имя, пожалуйста: ");
+            var userName = Console.ReadLine();
+            DateTime todayDate = DateTime.Today;
+            Console.WriteLine($"Привет, {userName}, сегодня {todayDate.ToShortDateString()}г.");
+        }
+    }
+}
