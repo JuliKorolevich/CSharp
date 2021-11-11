53bb5d959b7a7a79d2156fae56d606d7fc8d1fce
Julia Koroleva <julikorolevich94@gmail.com>
2021-11-11 23:32:04 +0300

    Added Lesson4. Tasks 1-3

---
+++ Lesson4_Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // запрос ввода чисел через пробел, формирование массива значений, где разделитель - пробел
+            Console.Write("Введите числа через пробел: ");
+            string line = Console.ReadLine();
+            string[] numbersByLine = line.Split(' ');
+
+            // преобразование строкового массива в целочисленный
+            int[] numbers = new int[numbersByLine.Length];
+
+            for (int i = 0; i < numbersByLine.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(numbersByLine[i]);
+            }
+
+            // подсчет суммы значений элементов массива numbers и вывод на экран пользователю
+            int sum;
+            sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
+            Console.WriteLine($"Сумма чисел: {sum}");
+
+            /* вывод последовательно каждого элемента массива numbers
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number}");
+            }  */
+        }
+    }
+}
