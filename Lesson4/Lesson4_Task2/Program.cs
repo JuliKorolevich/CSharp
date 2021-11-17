bfa433db20dbe6e8744c0fd950aa6e4b62d0cf49
Julia Koroleva <julikorolevich94@gmail.com>
2021-11-17 23:29:33 +0300

    deleted Lesson1 test folder
    This reverts commit 31ad599144a11d97f3e6f2bf99fe8d06ce43f69a.

---
+++ Lesson4/Lesson4_Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lesson4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // запрос ввода чисел через пробел, формирование массива значений, где разделитель - пробел
+            Console.Write("Введите числа через пробел: ");
+            string line = Console.ReadLine();
+            string number = "";
+            int sum = 0;
+
+            // обработать строку посимвольно: если не равно пробелу, то к строкеовой переменной number добавляем обрабатываемый символ, если равно, то
+            // если number не равен пустой строке, то увеличиваем сумму: считаем сумму, как предыдущая сумма плюс number (number конвертируем в число)
+            // иначе - присваем number пустую строку.
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char e = line[i];
+                if (e != ' ')
+                {
+                    number = number + e;
+                }
+                else
+                {
+                    sum = Sum(number, sum);
+                    number = "";
+                }
+            }
+            // вызов метода sum, чтобы прибавить последнее полученное число с помощью цикла for
+            sum = Sum(number, sum);
+
+            // вывод суммы в консоль
+            Console.WriteLine($"Сумма чисел: {sum}");
+        }
+
+        // метод увеличивает сумму путем сложения суммы и числа, полученного из строки путем конвертации строки в число
+        static int Sum(string number, int sum)
+        {
+            if (number != "")
+            {
+                sum = sum + Convert.ToInt32(number);
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ Lesson4/Lesson4_Task3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Lesson4_Task3
+{
+    class Program
+    {
+        enum Seasons
+        {
+            // времена года.
+            Winter = 1,
+            Spring,
+            Summer,
+            Autumn
+        }
+        static void Main(string[] args)
+        {
+            // вызов метода запроса номера месяца, вызов метода получения значения справочника Seasons, вызов метода
+            // получения названия времени года на русском языке, вывод названия времени года на экран
+            int currentMonthNumber = CurrentMonthNumber();
+            Seasons seasonEng = CurrentSeasonName(currentMonthNumber);
+            string seasonRusName = CurrentSeasonRusName(seasonEng);
+            Console.WriteLine(seasonRusName);
+
+        }
+         // метод запрашивает и возвращает порядковый номер текущего месяца
+        static int CurrentMonthNumber()
+        {
+            Console.Write("Введите порядковый номер текущего месяца: ");
+            int currentMonthNumber = int.Parse(Console.ReadLine());
+            return currentMonthNumber;
+        }
+
+        /* метод получает на вход число (номер месяца), в случае, если номер не входит в промежуток 1-12,
+         * вызывает метод запроса номера текущего месяца, а затем текущий метод повторно.
+         * В случае, если число входит в промежуток 1-12, то возвращает значение списка Seasons в зависимости от числа месяца */
+        static Seasons CurrentSeasonName(int MonthNumber)
+        {
+            if (MonthNumber >= 1 && MonthNumber <= 12)
+            {
+                Seasons season;
+                if (MonthNumber >= 3 && MonthNumber <= 5)
+                {
+                    season = Seasons.Spring;
+                    return season;
+                }
+                if (MonthNumber >= 6 && MonthNumber <= 8)
+                {
+                    season = Seasons.Summer;
+                    return season;
+                }
+                if (MonthNumber >= 9 && MonthNumber <= 11)
+                {
+                    season = Seasons.Autumn;
+                    return season;
+                }
+                else
+                {
+                    season = Seasons.Winter;
+                    return season;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите число от 1 до 12");
+                int month = CurrentMonthNumber();
+                return CurrentSeasonName(month);
+            }
+        }
+
+        // метод получает на вход справочное значение Seasons, возвращает название времени года на русском языке
+        static string CurrentSeasonRusName(Seasons MonthNumber)
+            {
+                string currentSeasonRusName;
+                if (MonthNumber == Seasons.Winter)
+                {
+                    currentSeasonRusName = "Зима";
+                    return currentSeasonRusName;
+                }
+                if (MonthNumber == Seasons.Spring)
+                {
+                    currentSeasonRusName = "Весна";
+                    return currentSeasonRusName;
+                }
+                if (MonthNumber == Seasons.Summer)
+                {
+                    currentSeasonRusName = "Лето";
+                    return currentSeasonRusName;
+                }
+                else
+                {
+                    currentSeasonRusName = "Осень";
+                    return currentSeasonRusName;
+                }
+            }
+    }
+}
