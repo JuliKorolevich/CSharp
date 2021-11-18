99cbfcfef77c54ed90f2808645a6539ef09258e3
Julia Koroleva <julikorolevich94@gmail.com>
2021-11-18 13:02:26 +0300

    Added Lesson4

---
+++ Lesson4_Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lesson4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // вызов методов по запросу имени, фамилии, отчества, а также по формированию ФИО (3 раза)
+            string name1 = GetfirstName();
+            string lastname1 = Getlastname();
+            string patronymic1 = Getpatronymic();
+            string fullname1 = GetfullName(name1, lastname1, patronymic1);
+            Console.WriteLine($"ФИО1: {fullname1}");
+            string name2 = GetfirstName();
+            string lastname2 = Getlastname();
+            string patronymic2 = Getpatronymic();
+            string fullname2 = GetfullName(name2, lastname2, patronymic2);
+            Console.WriteLine($"ФИО2: {fullname2}");
+            string name3 = GetfirstName();
+            string lastname3 = Getlastname();
+            string patronymic3 = Getpatronymic();
+            string fullname3 = GetfullName(name3, lastname3, patronymic3);
+            Console.WriteLine($"ФИО3: {fullname3}");
+        }
+        // метод запрашивает у пользователя и возвращает имя
+        static string GetfirstName()
+        {
+            Console.Write("Введите имя: ");
+            string firstname = Console.ReadLine();
+            return firstname;
+        }
+
+        // метод запрашивает у пользователя и возвращает фамилию
+        static string Getlastname()
+        {
+            Console.Write("Введите фамилию: ");
+            string lastname = Console.ReadLine();
+            return lastname;
+        }
+
+        // метод запрашивает у пользователя и возвращает отчество
+        static string Getpatronymic()
+        {
+            Console.Write("Введите отчество: ");
+            string patronymic = Console.ReadLine();
+            return patronymic;
+        }
+
+        // метод формирует и возвращает полное имя (фамилия + имя + отчество)
+        static string GetfullName(string firstName, string lastName, string patronymic)
+        {
+            string fullName = firstName + " " + lastName + " " + patronymic;
+            return fullName;
+        }
+
+
+    }
+}
---
+++ Lesson4_Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lesson4_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // просим ввести номер последовательности числа Фибоначчи, которое хотим получить
+            Console.Write("Введите номер числа Фибоначчи: ");
+            string fib = Console.ReadLine();
+            int fibNumber = Convert.ToInt32(fib);
+
+            // задаем первые два числа в последовательности Фибоначчи 0 и 1
+            int previousFibNumber = 2;
+            int firstNumber = 0;
+            int secondNumber = 1;
+
+            // проверяем, что указанное пользователем число больше 0, в противном случае выдаем ошибку
+            if (fibNumber > 0)
+            {
+                int number = GetFibonacci(firstNumber, secondNumber, fibNumber, previousFibNumber);
+                Console.WriteLine($"Число Фибоначчи: {number}");
+            }
+            else
+            {
+                Console.WriteLine($"Число должно быть больше 0.");
+            }
+
+        }
+
+        // метод расчета числа фибоначчи на основании предыдущего числа последовательноти, текущего, запрашиваемого номера последовательности Фибоначчи
+        // пользователем, а также номера последовательности предыдущего обработанного числа Фибоначчи
+        static int GetFibonacci(int previousNumber, int currentNumber, int fibNumber, int previousFibNumber)
+        {
+            int nextNumber;
+            if (previousFibNumber < fibNumber)
+            {
+                nextNumber = previousNumber + currentNumber;
+                previousFibNumber = previousFibNumber + 1;
+                return GetFibonacci(currentNumber, nextNumber, fibNumber, previousFibNumber);
+            }
+            if (fibNumber == 1)
+            {
+                return previousNumber;
+            }
+            else
+            {
+                return currentNumber;
+            }
+        }
+    }
+}
